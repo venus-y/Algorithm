c9e0a3a66dc74d3b6862d52a6a72a87a0c6e8342
venus01 <102720261+venus-y@users.noreply.github.com>
2024-08-10 14:25:03 +0900

    [Silver III] Title: 1로 만들기, Time: 108 ms, Memory: 15500 KB -BaekjoonHub

---
+++ Java/백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i == 1) {
+                dp[i] = 0;
+            }
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
